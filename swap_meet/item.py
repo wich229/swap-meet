3b0a24f1fb7c50f00dfdf07c48e11849f99abb3a
Eva Liu <una229@gmail.com>
2022-12-03 00:19:36 -0500

    wave2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,19 @@
+import uuid
+
 class Item:
-    pass
+    # composite class to component vendor
+    # attr: id = UUID_int, item
+    # methods: get_by_id, get_category(from vendor)
+
+    def __init__(self, id = 0):
+        if not id:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+    
+
+    def get_category(self):
+        return str(self.__class__.__name__)
+
+
+
