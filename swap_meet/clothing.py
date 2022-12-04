ee6d0677d8c0791c7b3c7de5b6a91f73a5036802
Eva Liu <una229@gmail.com>
2022-12-04 11:41:11 -0500

    wave 5 as item's class children class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,15 @@
-class Clothing:
-    pass
+from .item import Item
+# import uuid
+
+class Clothing(Item):
+    
+    def __init__(self, id = None, fabric = "Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+    
+    def __str__(self):
+        return f"{super().__str__()}. It is made from {self.fabric} fabric."
+
+    
+    # def get_category(self):
+    #     return str(self.__class__.__name__)
